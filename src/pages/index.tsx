11f1084e05e38a517aadba461e85edc2facaa6eb
Gloridust <Gloridust@gmail.com>
2025-07-07 19:09:22 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home({ initialData }: HomeProps) {
           <meta property="og:type" content="website" />
           <meta property="og:title" content={data.title} />
           <meta property="og:description" content={data.message} />
-          <meta property="og:image" content="https://edit-board.innovisle.net/favicon.ico" />
+          <meta property="og:image" content="https://edit-board.innovisle.net/favicon.png" />
           <meta property="og:url" content="https://edit-board.innovisle.net/" />
           
           <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -97,7 +97,7 @@ export default function Home({ initialData }: HomeProps) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={data.title} />
         <meta property="og:description" content={data.message} />
-        <meta property="og:image" content="https://edit-board.innovisle.net/favicon.ico" />
+        <meta property="og:image" content="https://edit-board.innovisle.net/favicon.png" />
         <meta property="og:url" content="https://edit-board.innovisle.net/" />
         
         <meta name="viewport" content="width=device-width, initial-scale=1" />
